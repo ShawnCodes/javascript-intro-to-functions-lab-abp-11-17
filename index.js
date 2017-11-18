420898b5436b41127089768536763e5c154394c6
shawn@huthunt.com <shawn@huthunt.com>
2017-11-18 23:07:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+return string.toUpperCase()
+}
+
+function whisper(string) {
+return string.toLowerCase()
+}
+
+function logShout(string){
+console.log(string.toUpperCase());}
+
+logshout("hello")();
+
+function logWhisper(string){
+console.log(string.toLowerCase());}
+
+
+
+function sayHiToGrandma(string){
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase
+if (lowercase) {
+  return "I can't hear you!"
+}
+}
+
+function sayHiToGrandma(string){
+  var uppercase = "HELLO!"
+  uppercase.toUpperCase() === uppercase
+  if (uppercase){
+  return "YES INDEED!"
+}
+}
