9bfd46cfaa45e221358759772384912deb2f2873
shawn@huthunt.com <shawn@huthunt.com>
2017-11-19 00:27:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,21 +16,20 @@ console.log(string.toLowerCase());}
 
 
 function sayHiToGrandma(string){
-var lowercase = 'hello!'
-lowercase.toLowerCase() === lowercase
-var uppercase = "HELLO!"
-uppercase.toUpperCase() === uppercase
-var mixedCase = "I love you, Grandma."
- mixedCase.toLowerCase() === mixedCase
- mixedCase.toUpperCase() === mixedCase
+//first use case: if string is all lower case, return "I can't hear you!"
+//second use case: if string is all upper case, return  "YES INDEED!"
+//third use case: if string is mixed case, return "I love you, too."
 
- if (lowercase) {
-   return "I can't hear you!"}
+  if (string.toLowerCase() === string){
+    return "I can't hear you!"
+  }
 
-   if (uppercase){
-   return "YES INDEED!"}
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
 
-   if (mixedCase) {
-     return "I love you, too."
-   }
- }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+//
